fb08e8877f9ece2a283cf29b97261a62899d8a1c
Carl Wright <c_wright125@hotmail.co.uk>
2026-02-10 19:27:20 +0000

    Initial working version of Dart Scorer

---
+++ DEBUG_NOTES.md
@@ -0,0 +1,27 @@
+#Debug Notes
+
+a running log of bigs, fixes, and lessons learned.
+
+### Template -
+
+### Bug:
+### Sympton:
+### Cause:
+### Fix:
+### Lesson:
+
+---
+
+### Bug: Bust condition never true
+### Sympton: bust() alert fired but if(bust()) never ran
+### Cause: bust() returned undefined instead of true
+### Fix: return true from bust()
+### Lesson: Functions used in if() must return a boolean (true/false)
+
+---
+
+### Bug: undoLastScore function is adding lastScore and currentScore like strings instead of integers
+### Sympton: e.g. 300 += 120 == 300120 instead of 420
+### Cause: userInput.value automatically turns an integer into a string
+### Fix: added "+" before lastScore to turn it back into integer. The problem never arrised on the minusScore function ass - automatically turns strings into integers so I never realised userInput.value was creating a string in the first place.
+### Lesson: taking .value turns integer into string. Use + before the variable to turn back into integer. MAIN LESSON: Number + Number = Number. String + anything = string. Number - number = number. Number - String = number.
---
+++ dartScorer.js
@@ -0,0 +1,228 @@
+const submit = document.getElementById("submit");
+const userInput = document.getElementById("userInput");
+const playerOneScore = document.getElementById("playerOneScore");
+const playerTwoScore = document.getElementById("playerTwoScore");
+const playerOneName = document.getElementById("playerOneName");
+const playerTwoName = document.getElementById("playerTwoName");
+const playerOneNameInput = document.getElementById("playerOneNameInput");
+const playerTwoNameInput = document.getElementById("playerTwoNameInput");
+const oneKOne = document.getElementById("1001");
+const fiveOOne = document.getElementById("501");
+const threeOOne = document.getElementById("301");
+const startButton = document.getElementById("start");
+const nameOne = document.getElementById("nameOne");
+const nameTwo = document.getElementById("nameTwo");
+const startGameDiv = document.getElementById("startGame");
+const display = document.getElementById("displayOne");
+const displayTwo = document.getElementById("displayTwo");
+const thrower = document.getElementById("thrower");
+const throwerButton = document.getElementById("throwerButton");
+const playerOneFirst = document.getElementById("playerOneFirst");
+const playerTwoFirst = document.getElementById("playerTwoFirst");
+const clearBtn = document.getElementById("clear");
+const undoBtn = document.getElementById("undo");
+
+
+let currentPlayer = 0;
+let playerThrowFirst = 0;
+let startingScore = 0;
+let pOneScore = 0;
+let pTwoScore = 0;
+let lastScore = 0;
+let pOneLegs = 0;
+let pTwoLegs = 0;
+
+/*function testUi() {
+    document.getElementById("test").innerText = currentPlayer;
+}*/
+
+function numBtnInput(value) {
+    userInput.value += value;
+}
+
+function undoLastScore() {
+    if(lastScore == 0) {
+        return;
+    } else {
+    if(currentPlayer == 1) {
+        pOneScore += +lastScore;
+        playerOneScore.innerHTML = pOneScore;
+        currentPlayer--;
+        marker();
+        lastScore = 0;
+        return;
+    } if(currentPlayer == 0) {
+        pTwoScore += +lastScore;
+        playerTwoScore.innerHTML = pTwoScore;
+        currentPlayer++;
+        marker();
+        lastScore = 0;
+        return;
+    }
+}
+}
+
+
+function startGame() {
+    playerOneName.innerText = playerOneNameInput.value;
+    playerTwoName.innerText = playerTwoNameInput.value;
+    nameOne.innerText = playerOneNameInput.value;
+    nameTwo.innerText = playerTwoNameInput.value;
+    if (oneKOne.checked) {
+        pOneScore += 1001;
+        pTwoScore += 1001;
+        playerOneScore.innerText = pOneScore;
+        playerTwoScore.innerText = pTwoScore;
+    }
+    if (fiveOOne.checked) {
+        pOneScore += 501;
+        pTwoScore += 501;
+        playerOneScore.innerText = pOneScore;
+        playerTwoScore.innerText = pTwoScore;
+        startingScore++;
+    }
+    if (threeOOne.checked) {
+        pOneScore += 301;
+        pTwoScore += 301;
+        playerOneScore.innerText = pOneScore;
+        playerTwoScore.innerText = pTwoScore;
+        startingScore += 2;
+    }
+    startGameDiv.style.display = "none";
+    thrower.style.display = "block";
+    noNames();
+
+}
+
+function noNames() {
+    if (playerOneNameInput.value == "") {
+        playerOneName.innerText = "Player One";
+        nameOne.innerText = "Player One";
+    }
+    if (playerTwoNameInput.value == "") {
+        playerTwoName.innerText = "Player Two";
+        nameTwo.innerText = "Player Two";
+    }
+}
+
+function whoThrows() {
+    if (playerTwoFirst.checked) {
+        currentPlayer++;
+    }
+    display.style.display = "grid";
+    displayTwo.style.display = "grid";
+    thrower.style.display = "none";
+    marker();
+}
+
+function marker() {
+    if (currentPlayer == 0) {
+        playerTwoName.style.backgroundColor = "white";
+        playerOneName.style.backgroundColor = "blue";
+
+    } if (currentPlayer == 1) {
+        playerTwoName.style.backgroundColor = "blue";
+        playerOneName.style.backgroundColor = "white";
+    }
+}
+
+function checkInvalidScore(score) {
+    if (score > 180) {
+        alert("Too High!");
+        userInput.value = "";
+        return true;
+    } if (score < 0) {
+        alert("Impossible!");
+        userInput.value = "";
+        return true;
+    }
+}
+
+function bust() {
+    let x = pOneScore - userInput.value;
+    let y = pTwoScore - userInput.value;
+    if((x < 0 || x == 1) && currentPlayer == 0) {
+        userInput.value = "";
+        currentPlayer++;
+        marker();
+        alert("P1 Bust!");
+        return true;
+    } if((y < 0 || y == 1) && currentPlayer == 1) {
+        userInput.value = "";
+        currentPlayer--;
+        marker();
+        alert("P2 Bust!");
+        return true;
+    }
+}
+
+function newLeg() {
+    if (startingScore == 0) {
+        pOneScore = 1001;
+        playerOneScore.innerHTML = pOneScore;
+        pTwoScore = 1001;
+        playerTwoScore.innerHTML = pTwoScore;
+    }
+    if (startingScore == 1) {
+        pOneScore = 501;
+        playerOneScore.innerHTML = pOneScore;
+        pTwoScore = 501;
+        playerTwoScore.innerHTML = pTwoScore;
+    }
+    if (startingScore == 2) {
+        pOneScore = 301;
+        playerOneScore.innerHTML = pOneScore;
+        pTwoScore = 301;
+        playerTwoScore.innerHTML = pTwoScore;
+    }
+} // Start refractoring all code before adding anymore features.//
+
+
+function winGame() {
+    if(currentPlayer == 1 && pOneScore == 0) {
+        pOneLegs++;
+        document.getElementById("playerOneLegs").innerHTML = pOneLegs;
+        newLeg();
+        alert("Player one wins!");
+    } if(currentPlayer == 0 && pTwoScore == 0) {{
+        pTwoLegs++;
+        document.getElementById("playerTwoLegs").innerHTML = pTwoLegs;
+        newLeg();
+        alert("Player two wins!");
+    }}
+}
+
+function minusScore() {
+    if (checkInvalidScore(userInput.value)) {
+        return;
+    } 
+    if(bust()) {
+        return;
+    }
+    else if (currentPlayer == 0) {
+        lastScore = userInput.value;
+        pOneScore -= userInput.value;
+        playerOneScore.innerHTML = pOneScore;
+        currentPlayer++;
+        marker();
+
+    } else if (currentPlayer == 1) {
+        lastScore = userInput.value;
+        pTwoScore -= userInput.value;
+        playerTwoScore.innerHTML = pTwoScore;
+        currentPlayer--;
+        marker();
+    }
+    userInput.value = "";
+    winGame();
+}
+
+function clear() {
+    userInput.value = "";
+}
+
+startButton.addEventListener("click", startGame);
+throwerButton.addEventListener("click", whoThrows);
+submit.addEventListener("click", minusScore);
+clearBtn.addEventListener("click", clear);
+undoBtn.addEventListener("click", undoLastScore);
